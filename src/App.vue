0ad7da3c12498a2c0dd48adfa3d9f11bfadc0763
Yuka Suzuki <yuka.kurihara821@gmail.com>
2024-05-07 19:55:25 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <Header />
+  <div class="container">
+    <Balance />
+    <IncomeExpenses />
+    <TransactionList :transactions="transactions" />
+    <AddTransaction />
+  </div>
+</template>
+
+<script setup>
+import Header from './components/Header.vue';
+import Balance from './components/Balance.vue';
+import IncomeExpenses from './components/IncomeExpenses.vue';
+import TransactionList from './components/TransactionList.vue';
+import AddTransaction from './components/AddTransaction.vue';
+
+import { ref } from 'vue';
+
+const transactions = ref([
+  { id: 1, text: 'flower', amount: -19.99 },
+  { id: 2, text: 'salary', amount: 299.97 },
+  { id: 3, text: 'book', amount: -10 },
+  { id: 4, text: 'camera', amount: 150 },
+]);
+</script>
---
+++ src/components/TransactionList.vue
@@ -0,0 +1,13 @@
+<template>
+  <h3>History</h3>
+  <ul id="list" class="list">
+    <li
+      v-for="transaction in transactions"
+      :key="transaction.id"
+      :class="transaction.amount < 0 ? 'minus' : 'plus'"
+    >
+      {{ transaction.text }} <span>${{ transaction.amount }}</span
+      ><button class="delete-btn">x</button>
+    </li>
+  </ul>
+</template>
